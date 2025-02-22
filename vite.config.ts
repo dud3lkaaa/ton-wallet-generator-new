72e678311320355a383878ce2505fe9f7eeff9c2
dud3lkaaa <141063094+dud3lkaaa@users.noreply.github.com>
2025-02-22 16:30:01 +0300

    Deploy TON wallet generator to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/ton-wallet-generator-new/', // Имя твоего репозитория
+  base: '/ton-wallet-generator-new/', // Имя репозитория
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
